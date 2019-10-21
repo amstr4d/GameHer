2a60ee7f842f77fbf30c7a1d0ab5530fc10305ca
Noëlie <noelieroux11@gmail.com>
2019-10-21 09:25:52 +0200

    Revert "- Stop email from being saved to file, send straight away and handle …"

--- src/Controller/TeamController.php
+++ src/Controller/TeamController.php
@@ -51,12 +51,9 @@ class TeamController extends AbstractController
                 ->setTo('contact@gameher.fr')
                 ->setBody($data['message']);
 
-            try {
-                $mailer->send($message);
-                $this->addFlash('success', 'message sent');
-            } catch(\Swift_TransportException $e) {
-                $this->addFlash('danger', 'message could not be sent');
-            }
+            $mailer->send($message);
+
+            $this->addFlash('success', 'message sent');
         }
 
         if ($form->isSubmitted() && !$form->isValid()) {
